8f1088dcd56c7100b73d1316dc13dade75e4c79a
MickaelLepicier <mickael233@gmail.com>
2025-03-18 15:59:47 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import{} from ''
+import{} from ''
+import cookieParser from 'cookie-parser'
+
+const app = express()
+app.use(express.static('public'))
+app.use(cookieParser())
+
+app.get('/', (req,res)=> res.send('Hello there'))
+app.listen(3030, ()=> console.log('Server ready at port 3030'))
+
